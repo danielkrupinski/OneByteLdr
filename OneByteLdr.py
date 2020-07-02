115b06bebef41a9bdf407aed049f5051b03039fd
Daniel Krupiński <danielkrupinski@outlook.com>
2020-07-02 12:05:24 +0200

    Bypass detection of thread creation

--- OneByteLdr.py
+++ OneByteLdr.py
@@ -2,12 +2,27 @@ import pymem
 import re
 
 pm = pymem.Pymem('csgo.exe')
+
+# bypass NtOpenFile hook in csgo.exe
+
 csgo = pymem.process.module_from_name(pm.process_handle,
-                                        'csgo.exe')
+                                      'csgo.exe')
 
 csgoModule = pm.read_bytes(csgo.lpBaseOfDll, csgo.SizeOfImage)
 address = csgo.lpBaseOfDll + re.search(rb'.\x1A\xF6\x45\x0C\x20',
-                                         csgoModule).start()
+                                       csgoModule).start()
 
 pm.write_uchar(address, 0xEB if pm.read_uchar(address) == 0x74 else 0x74)
+
+# bypass thread creation detection in DllMain of client.dll
+
+client = pymem.process.module_from_name(pm.process_handle,
+                                        'client.dll')
+
+clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
+address = client.lpBaseOfDll + re.search(rb'.\x55\x6A\x00\x6A\x04',
+                                         clientModule).start()
+
+pm.write_uchar(address, 0xEB if pm.read_uchar(address) == 0x74 else 0x74)
+
 pm.close_process()

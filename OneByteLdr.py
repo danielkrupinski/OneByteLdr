b19a939c127de313eb50ad09231529e26ee63584
rakion99 <_rakion99@outlook.com>
2020-09-08 23:11:49 -0500

    Update OneByteLdr.py

--- OneByteLdr.py
+++ OneByteLdr.py
@@ -9,7 +9,7 @@ csgo = pymem.process.module_from_name(pm.process_handle,
                                       'csgo.exe')
 
 csgoModule = pm.read_bytes(csgo.lpBaseOfDll, csgo.SizeOfImage)
-address = csgo.lpBaseOfDll + re.search(rb'.\x1A\xF6\x45\x0C\x20',
+address = csgo.lpBaseOfDll + re.search(rb'.\x1B\xF6\x45\x0C\x20',
                                        csgoModule).start()
 
 pm.write_uchar(address, 0xEB if pm.read_uchar(address) == 0x74 else 0x74)
